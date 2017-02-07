9aeecd6f68dbb511796391701f23379d0bbb5fa8
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-02-07 18:59:46 +0530

    Tie everything up in app.core to make stuff work.

---
+++ src/cljs/app/core.cljs
@@ -0,0 +1,29 @@
+(ns app.core
+    (:require [reagent.core :as reagent]
+              [re-frame.core :as re-frame]
+              [re-frisk.core :refer [enable-re-frisk!]]
+              [app.routes :as routes]
+              [app.config :as config]
+              [app.views :as views]
+              [app.events] [app.subs]
+              [app.home.events] [app.home.subs]
+              ))
+
+
+(defn dev-setup []
+  (when config/debug?
+    (enable-console-print!)
+    (enable-re-frisk!)
+    (println "dev mode")))
+
+
+(defn mount-root []
+  (re-frame/clear-subscription-cache!)
+  (reagent/render [views/root-view]
+                  (.getElementById js/document "app")))
+
+(defn ^:export init []
+  (routes/app-routes)
+  (re-frame/dispatch-sync [:initialize-db])
+  (dev-setup)
+  (mount-root))
